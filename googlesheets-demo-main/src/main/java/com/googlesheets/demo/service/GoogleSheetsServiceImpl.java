a75cd9cb0fbb712c27cdda446d1baf7d5a63c64d
nghiatq <nghiatq@vmogroup.com>
2024-01-30 16:22:27 +0700

    init

---
+++ googlesheets-demo-main/src/main/java/com/googlesheets/demo/service/GoogleSheetsServiceImpl.java
@@ -0,0 +1,80 @@
+package com.googlesheets.demo.service;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
+import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
+import com.google.api.services.sheets.v4.model.Sheet;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.googlesheets.demo.config.GoogleAuthorizationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class GoogleSheetsServiceImpl implements GoogleSheetsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GoogleSheetsServiceImpl.class);
+
+    @Value("${spreadsheet.id}")
+    private String spreadsheetId;
+
+    @Autowired
+    private GoogleAuthorizationConfig googleAuthorizationConfig;
+
+    @Override
+    public void getSpreadsheetValues() throws IOException, GeneralSecurityException, FileNotFoundException {
+        Sheets sheetsService = googleAuthorizationConfig.getSheetsService();
+        Sheets.Spreadsheets.Values.BatchGet request =
+                sheetsService.spreadsheets().values().batchGet(spreadsheetId);
+        request.setRanges(getSpreadSheetRange());
+        request.setMajorDimension("ROWS");
+        BatchGetValuesResponse response = request.execute();
+        List<List<Object>> spreadSheetValues = response.getValueRanges().get(0).getValues();
+        List<Object> headers = spreadSheetValues.remove(0);
+
+        String RANGE = "Sheet1";
+
+        // Define the data to append
+        String[] data = new String[]{"Honda", "BMW", "Ford", "Mazda"};
+        List<List<Object>> dataToAppend = Arrays.asList(
+                Arrays.asList("New Data 1", "New Data 2"),
+                Arrays.asList("More New Data 1", "More New Data 2"));
+
+        // Create the ValueRange
+        ValueRange body = new ValueRange()
+                .setValues(dataToAppend);
+
+        // Append the data
+        AppendValuesResponse result = sheetsService.spreadsheets().values()
+                .append(spreadsheetId, RANGE, body)
+                .setValueInputOption("RAW")
+                .execute();
+
+        // Print the updated range
+        System.out.printf("Appended %d rows.\n", result.getUpdates().getUpdatedCells());
+
+
+    }
+
+    private List<String> getSpreadSheetRange() throws IOException, GeneralSecurityException {
+        Sheets sheetsService = googleAuthorizationConfig.getSheetsService();
+        Sheets.Spreadsheets.Get request = sheetsService.spreadsheets().get(spreadsheetId);
+        Spreadsheet spreadsheet = request.execute();
+        Sheet sheet = spreadsheet.getSheets().get(0);
+        int row = sheet.getProperties().getGridProperties().getRowCount();
+        int col = sheet.getProperties().getGridProperties().getColumnCount();
+        return Collections.singletonList("R1C1:R".concat(String.valueOf(row))
+                .concat("C").concat(String.valueOf(col)));
+    }
+
+}
